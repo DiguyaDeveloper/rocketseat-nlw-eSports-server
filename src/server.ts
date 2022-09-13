c31f0f236a499ee4c3888d3d5d24e2c2b1dbce9c
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-09-13 20:36:35 -0300

    feat: :sparkles: introduce first endpoint rest

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.get("/ads", (request, response) => {
+  return response.json([
+    { id: 1, name: "Anuncio 1" },
+    { id: 2, name: "Anuncio 2" },
+  ]);
+});
+
+app.listen(3333);
