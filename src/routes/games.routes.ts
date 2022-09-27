2b7598bf62c99072a7e2228e6373d63530fd002c
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-09-27 19:29:33 -0300

    feat: :sparkles: Update routes of games
    Create get games
    Create get Ads of Games
    Create post Ads of Games and Include adWeekDays because have a relational tables in weekDays and Ads games

--- src/routes/games.routes.ts
+++ src/routes/games.routes.ts
@@ -1,20 +1,126 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+import { convertHourStringToMinutes } from "../shared/utils/convert-timing.utils";
 
 const routes = express();
-
-routes.post("/games", (request, response) => {
-  return response.json([]);
+const prisma = new PrismaClient({
+  log: ["query"],
 });
 
-routes.get("/games", (request, response) => {
-  return response.json([]);
+routes.get("/games", async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      Ads: {
+        select: {
+          discord: true,
+          _count: {
+            select: {
+              AdWeekDays: true,
+            },
+          },
+        },
+      },
+      _count: {
+        select: {
+          Ads: true,
+        },
+      },
+    },
+  });
+  return response.status(200).json(games);
 });
 
-routes.get("/games/:id/ads", (request, response) => {
-  return response.json([
-    { id: 1, name: "Anuncio 1" },
-    { id: 2, name: "Anuncio 2" },
-  ]);
+routes.get("/games/:id/ads", async (request, response) => {
+  const gameId = request.params.id;
+
+  const ads = await prisma.ads.findMany({
+    select: {
+      id: true,
+      name: true,
+      useVoiceChannel: true,
+      yearPlaying: true,
+      hourStart: true,
+      hourEnd: true,
+      createdAt: true,
+      AdWeekDays: {
+        select: {
+          weekDays: {
+            select: {
+              dayNumber: true,
+              day: true,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      },
+    },
+    where: {
+      gameId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return response.json(ads);
+});
+
+routes.post("/games/:id/ads", async (request, response) => {
+  const gameId: { id: string } = await prisma.game.findFirstOrThrow({
+    select: {
+      id: true,
+    },
+    where: {
+      id: request.params.id,
+    },
+  });
+
+  if (!gameId || !gameId.id) {
+    return response
+      .status(404)
+      .json({ error: `Game not found with id: ${gameId}` });
+  }
+
+  const gameAds = request.body;
+
+  const weekDayIds: { id: string }[] = await prisma.weekDays.findMany({
+    select: {
+      id: true,
+    },
+    where: {
+      dayNumber: { in: gameAds.weekDays },
+    },
+  });
+
+  if (!weekDayIds || !weekDayIds.length) {
+    return response.status(404).json({ error: `WeekDays inputed dont exists` });
+  }
+
+  const weekDaysToCreate = weekDayIds.map((value: { id: string }) => ({
+    weekDays: {
+      connect: {
+        id: value.id,
+      },
+    },
+  }));
+
+  const gameAdsCreated = await prisma.ads.create({
+    data: {
+      gameId: gameId.id,
+      name: gameAds.name,
+      yearPlaying: gameAds.yearPlaying,
+      discord: gameAds.discord,
+      AdWeekDays: {
+        create: weekDaysToCreate,
+      },
+      hourStart: convertHourStringToMinutes(gameAds.hourStart),
+      hourEnd: convertHourStringToMinutes(gameAds.hourEnd),
+      useVoiceChannel: gameAds.useVoiceChannel,
+    },
+  });
+
+  return response.status(201).json(gameAdsCreated);
 });
 
 routes.delete("/games", (request, response) => {
