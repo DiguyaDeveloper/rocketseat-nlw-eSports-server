19784f317afd11404344595b663cb07644db0497
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-09-27 19:28:11 -0300

    feat: :sparkles: Update routes of ads to list discord

--- src/routes/ads.routes.ts
+++ src/routes/ads.routes.ts
@@ -1,6 +1,11 @@
+import { PrismaClient } from "@prisma/client";
 import express from "express";
 
 const routes = express();
+
+const prisma = new PrismaClient({
+  log: ["query"],
+});
 
 routes.get("/ads/:id", (request, response) => {
   return response.json([
@@ -9,11 +14,20 @@ routes.get("/ads/:id", (request, response) => {
   ]);
 });
 
-routes.get("/ads/:id/discord", (request, response) => {
-  return response.json([
-    { id: 1, name: "Anuncio 1" },
-    { id: 2, name: "Anuncio 2" },
-  ]);
+routes.get("/ads/:id/discord", async (request, response) => {
+  const adsId = request.params.id;
+
+  const ads = await prisma.ads.findUniqueOrThrow({
+    select: {
+      discord: true,
+    },
+    where: {
+      id: adsId,
+    },
+  });
+  return response.json({
+    discord: ads.discord,
+  });
 });
 
 routes.post("/ads", (request, response) => {
